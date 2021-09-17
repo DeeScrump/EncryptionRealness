0091a699f5118b9fc1fa09341a5d341dece2e91e
DCKII <dckornbluthii@bellsouth.net>
2021-09-17 16:42:33 -0400

    commit changes to js code that include comments for pre-final release

--- script.js
+++ script.js
@@ -10,110 +10,103 @@ charactersLength = 0;
 
 // Write password to the #password input
 function writePassword() {
+
+  // Get user input for the number of characters the password will need to be, within restrictions
   var charLength = function () {
     // Ask user for their choice
     userNumber = window.prompt(
       "To generate your random password, first advise how many characters should it be?\n\n(requires a number between 8 and 128)"
     );
-    // If user pressed Cancel, immediately end function
     if (!userNumber) {
-      console.log(userNumber);
       return;
     } else if (userNumber < 8 || userNumber > 128) {
       window.alert("please choose a value between 8 and 128");
       charLength();
     } else {
-      console.log(userNumber);
       return;
     }
   };
   charLength();
 
-  u = function () {
+  // Get user input for whether they want capital letters to be included in the password.  Limited response to only y or n and when a positive response is received, add the list of available characters for the characters string.
+  var upperCase = function () {
+    // Ask user for their choice
     userUpper = window.prompt(
       "Would you like any 'CAPITAL' letters to be used?\n\n(requires y or n response)"
     );
-
     if (!userUpper) {
-      console.log(userUpper);
       return;
     } else if (userUpper == "Y" || userUpper == "y") {
-      console.log(userUpper);
       characters = characters + "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
       window.alert("got it");
     } else if (userUpper == "N" || userUpper == "n") {
-      console.log(userUpper);
       window.alert("no capital letters, got it");
     } else if (userUpper == null) {
-      console.log(userUpper);
       window.alert("please respond y or n");
       y();
     } else {
-      console.log(userUpper);
       window.alert("please respond y or n");
       y();
       return;
     }
   };
-  u();
+  upperCase();
 
-  l = function () {
+  // Get user input for whether they want lowercase letters to be included in the password.Limited response to only y or n and when a positive response is received, add the list of available characters for the characters string.
+  var lowerCase = function () {
+    // Ask user for their choice
     userLower = window.prompt(
       "Would you like any 'lowercase' letters to be used?\n\n(requires y or n response)"
     );
-
     if (!userLower) {
-      console.log(userLower);
       return;
     } else if (userLower == "Y" || userLower == "y") {
-      console.log(userLower);
       characters = characters + "abcdefghijklmnopqrstuvwxyz";
       window.alert("got it");
     } else if (userLower == "N" || userLower == "n") {
-      console.log(userLower);
       window.alert("no lowercase letters, got it");
     } else if (userLower == null) {
-      console.log(userLower);
       window.alert("please respond y or n");
       y();
     } else {
-      console.log(userLower);
       window.alert("please respond y or n");
       y();
       return;
     }
   };
-  l();
+  lowerCase();
 
-  s = function () {
+  // Get user input for whether they want special characters to be included in the password. Limited response to only y or n and when a positive response is received, add the list of available characters for the characters string.
+  var specialCharacter = function () {
+    // Ask user for their choice
     userCharSpec = window.prompt(
       "Would you like any 'special' characters to be used?\n\n(requires y or n response)"
     );
-
-    if (!userCharSpec) {
-      console.log(userCharSpec);
+      if (!userCharSpec) {
       return;
     } else if (userCharSpec == "Y" || userCharSpec == "y") {
-      console.log(userCharSpec);
-      characters = characters + "!@#$%^&*()";
+      characters = characters + " !#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
       window.alert("got it");
     } else if (userCharSpec == "N" || userCharSpec == "n") {
-      console.log(userCharSpec);
       window.alert("no special characters, got it");
     } else if (userCharSpec == null) {
-      console.log(userCharSpec);
       window.alert("please respond y or n");
       y();
     } else {
-      console.log(userCharSpec);
       window.alert("please respond y or n");
       y();
       return;
     }
   };
-  s();
+  specialCharacter();
+
+  // Write password to the #password input
+  var password = generatePassword(userNumber);
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
 }
 
+//Generate a randome password with parameter inputs from user
 function generatePassword(userNumber) {
   var result = "";
   characters = characters;
@@ -121,15 +114,8 @@ function generatePassword(userNumber) {
   for (var i = 0; i < userNumber; i++) {
     result += characters.charAt(Math.floor(Math.random() * charactersLength));
   }
-  var password = generatePassword(userNumber);
-  var passwordText = document.querySelector("#password");
-  passwordText.value = password;
   return result;
 }
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-// console.log(characters);
-// console.log(characters.length);
-// console.log(userNumber);
-// console.log(generatePassword(userNumber))
