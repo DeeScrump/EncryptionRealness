263ac479b1860964a7506308eb3a5c91596834a1
DCKII <dckornbluthii@bellsouth.net>
2021-09-17 13:58:12 -0400

    pushed multiple changes at once, function for choosing number, function for chooseing cap option, funcion for choosing lowercase option, function for choosing special characters

--- script.js
+++ script.js
@@ -1,18 +1,144 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+//   var charLength = function() {
+//     // Ask user for their choice
+//     var userNumber = window.prompt("To generate your random password, first advise how many characters should it be?\n\n(requires a number between 8 and 128)");
+//     // If user pressed Cancel, immediately end function
+//     if (!userNumber) {
+//       return;
+//     } else if (userNumber < 8 || userNumber > 128) {
+//       window.alert("please choose a value between 8 and 128");
+//       charLength();
+//     } else {
+//       console.log(userNumber);
+//       return;
+//     }
+//   }
+// charLength();
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-  var generatePassword = window.prompt("To generate your random pass, first advise how many characters should it be?");
-
-  passwordText.value = password;
-
+u = function () {
+  var userUpper = window.prompt("Would you like any 'CAPITAL' letters to be used?\n\n(requires y or n response)");
+  
+  if (!userUpper) {
+    console.log(userUpper);
+    return;
+  } else if (userUpper == 'Y' || userUpper == 'y') {
+    console.log(userUpper);
+    window.alert("got it");
+  } else if (userUpper == 'N' || userUpper == 'n') {
+    console.log(userUpper);
+    window.alert("no capital letters, got it");
+  } else if (userUpper == null) {
+    console.log(userUpper);
+    window.alert("please respond y or n")
+    y();
+  } else {
+    console.log(userUpper);
+    window.alert("please respond y or n")
+    y();
+    return;
+  }
 }
+u();
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+l = function () {
+  var userLower = window.prompt("Would you like any 'lowercase' letters to be used?\n\n(requires y or n response)");
+  
+  if (!userLower) {
+    console.log(userLower);
+    return;
+  } else if (userLower == 'Y' || userLower == 'y') {
+    console.log(userLower);
+    window.alert("got it");
+  } else if (userLower == 'N' || userLower == 'n') {
+    console.log(userLower);
+    window.alert("no lowercase letters, got it");
+  } else if (userLower == null) {
+    console.log(userLower);
+    window.alert("please respond y or n")
+    y();
+  } else {
+    console.log(userLower);
+    window.alert("please respond y or n")
+    y();
+    return;
+  }
+}
+l();
+
+s = function () {
+  var userCharSpec = window.prompt("Would you like any 'special' characters to be used?\n\n(requires y or n response)");
+  
+  if (!userCharSpec) {
+    console.log(userCharSpec);
+    return;
+  } else if (userCharSpec == 'Y' || userCharSpec == 'y') {
+    console.log(userCharSpec);
+    window.alert("got it");
+  } else if (userCharSpec == 'N' || userCharSpec == 'n') {
+    console.log(userCharSpec);
+    window.alert("no special characters, got it");
+  } else if (userCharSpec == null) {
+    console.log(userCharSpec);
+    window.alert("please respond y or n")
+    y();
+  } else {
+    console.log(userCharSpec);
+    window.alert("please respond y or n")
+    y();
+    return;
+  }
+}
+s();
+
+// var userCharacterSelect = function () {
+// }
+// userCharacterSelect();
 
 
-console.log(generatePassword);
+
+
+
+
+
+
+
+// // Assignment Code
+// var generateBtn = document.querySelector("#generate");
+// var userLength = window.prompt("How many characters for your password?\n(It must be between 8 and 128 characters)");
+
+// // Write password to the #password input
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
+
+//     generatePassword(charLength) {
+//         var result           = '';
+//         var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+//         var charLength = characters.length;
+//         for ( var i = 0; i < length; i++ ) {
+//           result += characters.charAt(Math.floor(Math.random() * 
+//       charactersLength));
+//       }
+//       return result;
+//     }
+
+//   passwordText.value = password;
+
+// }
+
+// // Add event listener to generate button
+// // charLength();
+// generateBtn.addEventListener("click", writePassword);
+
+
+// // function makeid(length) {
+// //   var result           = '';
+// //   var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+// //   var charactersLength = characters.length;
+// //   for ( var i = 0; i < length; i++ ) {
+// //     result += characters.charAt(Math.floor(Math.random() * 
+// // charactersLength));
+// //  }
+// //  return result;
+// // }
+
+// // console.log(makeid(5));
